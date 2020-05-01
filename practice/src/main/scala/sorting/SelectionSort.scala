26d6a99bd3dc45900510754b9bf43241a4da5920
jneo8 <jneo8lin@gmail.com>
2020-05-01 14:14:43 +0800

    Selection sort

---
+++ practice/src/main/scala/sorting/SelectionSort.scala
@@ -0,0 +1,24 @@
+package sorting
+
+object SelectionSortAscApp {
+  def main(args: Array[String]): Unit = {
+    println(selectionSortAsc(List(5, 3, 2, 8, 7)))
+    println(selectionSortAsc(List("k", "d", "c", "a", "q")))
+  }
+
+  def selectionSortAsc[T <% Ordered[T]](myData: List[T]):
+    List[T] = myData match {
+      case Nil => Nil
+      case head :: Nil => List(head)
+      case head :: tail => {
+        val minElem = tail.min
+        val indexOfMinElem = tail.indexOf(minElem)
+        if (head <= minElem) {
+          head :: selectionSortAsc(tail)
+        } else {
+          val (tailHalf1, tailHalf2) = tail.splitAt(indexOfMinElem)
+          tailHalf2.head :: selectionSortAsc(tailHalf1 ::: head :: tailHalf2.tail)
+        }
+      }
+    }
+}
